bf9edd7baa591b97674b1cf950d0b9220194b2d8
rajatk-31 <rajat.fastnfurious@gmail.com>
2017-08-17 16:24:50 +0530

    okok

--- public/js/app1.js
+++ public/js/app1.js
@@ -48,15 +48,12 @@ app.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 });
 
-<<<<<<< HEAD
-app.controller('ActorsController', function($http, $scope, $rootScope) {
-=======
+
 app.controller('LoginController', function($http, $scope) {
     console.log("Login Page");
 });
 
 app.controller('ActorsController', function($http, $scope) {
->>>>>>> afa4f852d5175b4eed791bac2b3759b17e111fd5
     $scope.refresh = function() {
         $http({
             url: 'api/actor/all'
